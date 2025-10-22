41cfc33ac92d7ca9433133104b175b289811e26a
xenodium <8107219+xenodium@users.noreply.github.com>
2025-10-22 18:45:19 +0100

    Adding Version and Package-Requires

--- time-zones.el
+++ time-zones.el
@@ -4,6 +4,8 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/time-zones
+;; Version: 0.3.2
+;; Package-Requires: ((emacs "29.1"))
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
